c6368b31b235bce3f60b2d32b931c3e6ef1c8c71
Jeremy Neiman <docmarionum1@gmail.com>
2016-06-09 20:24:23 -0400

    Code theoretically working, but havent tested on hardware

--- pet_trick/pet_trick.ino
+++ pet_trick/pet_trick.ino
@@ -2,20 +2,62 @@
 
 #define LED_PIN 6
 #define NUM_LEDS 30
+#define NUM_ROWS 6
+#define DELAY 50
 
-
+/*
+ * Button/LED Layout
+ * Each row is a a softpot
+ * arranged from min->max readings on the softpot
+ * and each number is the button index in the neopixel chain
+ * 
+ * 0  1  2  3  4  5
+ * 11 10 9  8  7  6
+ * 12 13 14 15 16 17
+ * 23 22 21 20 19 18
+ * 24 25 26 27 28 19
+ * 
+ * The first row goes into A0, the second in A1 and so forth, 
+ * with the fifth going to A4.
+ * 
+ * The neopixel data in pin goes to pin 6.
+ */
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+// The current colors of the LEDs
 byte colors[NUM_LEDS][3];
 
-void initColors() {
+// The state of the buttons
+bool buttonState[NUM_LEDS];
+
+// The threshold values for the softpots to register a button press
+const int thresholds[6][2] = {
+    {100, 120},
+    {180, 220},
+    {300, 360},
+    {400, 550},
+    {600, 720},
+    {850, 970} 
+  };
+
+/*
+ * Init Stuff that needs initing
+ */
+void initStuff() {
   for (int i = 0; i < NUM_LEDS; i++) {
+    // Set the starting colors to a medium brightness white
     for (int j = 0; j < 3; j++) {
       colors[i][j] = 127;
     }
+
+    // Set the button states to false
+    buttonState[i] = false;
   }
 }
 
+/*
+ * Apply the current colors array to the neopixels
+ */
 void setColors() {
   for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(
@@ -29,10 +71,13 @@ void setColors() {
  */
 void modifyColors(int n) {
   for (int i = 0; i < NUM_LEDS; i++) {
+    // Generate some "random" rgb offsets
     byte r = n*17 + i*37;
     byte g = n*91 + i*52;
     byte b = n*31 + i*123;
 
+    // If the button was odd, add the rgb values,
+    // Else subtract them
     if (n % 2 == 1) {
       colors[i][0] += r;
       colors[i][1] += g;
@@ -45,32 +90,47 @@ void modifyColors(int n) {
   }
 }
 
-int detectButton(int row) {
+/*
+ * Get the state of a button
+ * button is the index of the button from 0-29
+ * See top of file for details on button/LED layout.
+ * 
+ */
+bool detectButton(int button) {
+  // Get the row the button is on based on it's index
+  int row = button / NUM_ROWS;
+
+  // Read the value of the softpot for that row
   int val = analogRead(row);
 
-  Serial.println(val);
+  // Debugging
+  //Serial.println(val);
 
-  if (val > 100 && val < 120) {
-    return 0;
-  } else if (val > 180 && val < 220) {
-    return 1;
-  } else if (val > 300 && val < 360) {
-    return 2;
-  } else if (val > 400 && val < 550) {
-    return 3;
-  } else if (val > 600 && val < 720) {
-    return 4;
-  } else if (val > 850 && val < 970) {
-    return 5;
+  // Figure out the LED's position along the softpot 
+  // For even rows the higher the index, the further along the
+  // softpot the LED will be.  For odd rows it's reversed.
+  int idx = button % NUM_ROWS;
+
+  // If row is odd
+  if (row % 2 == 1) {
+    //Reverse the index
+    // 0 -> 5, 1 -> 4, 2 -> 3, 3 -> 2, 4 -> 1, 5 -> 0
+    idx = 5 - idx;
   }
 
-  return -1;
+  // Debugging
+  //Serial.println((String)"button = " + button + (String)" row = " + row + (String)" idx = " + idx);
+  //return random(0,2);
+
+  if (val > thresholds[idx][0] && val < thresholds[idx][1]) {
+    return true;
+  }
+
+  return false;
 }
 
 void setup() {
-  // put your setup code here, to run once:
-
-  initColors();
+  initStuff();
 
   strip.begin();
   strip.setBrightness(16);
@@ -81,23 +141,20 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  /*modifyColors(random(0, NUM_LEDS-1));
-  setColors();
-  strip.show();*/
-
-  int led = detectButton(5);
-  if (led > -1) {
-    //strip.setPixelColor(led, 255);
-    modifyColors(led);
-    setColors();
+  // Loop through all the LEDS/buttons and see whether they are being pressed.
+  // If so, and the state has changed, update the colors and save the state.
+  for (int i = 0; i < NUM_LEDS; i++) {
+    bool newState = detectButton(i);
+    if (newState && !buttonState[i]) {
+      modifyColors(i);
+    }
+    buttonState[i] = newState;
   }
 
+  // Set the colors to the neopixels and the refresh them
+  setColors();
   strip.show();
 
-  //Serial.println(analogRead(5));
-
-  delay(50);
+  delay(DELAY);
 
 }
