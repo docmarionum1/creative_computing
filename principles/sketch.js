502b824cdb6a3cc8f2c26db7866703862e889ead
Jeremy Neiman <docmarionum1@gmail.com>
2016-06-14 16:04:02 -0400

    finish with 1

--- principles/sketch.js
+++ principles/sketch.js
@@ -18,86 +18,50 @@ function draw() {
   pointLight(255, 255, 255, 10, 1, -1);
   ambientMaterial(255);
 
-
   velocity += a*dir;
   pos += velocity;
   momentum = mass*velocity;
 
-
-
   if (pos < groundPos) {
-    //console.log(velocity);
-    //console.log(momentum);
     pos = groundPos;
     dir = 1;
-    //scale(100/velocity, )
-    /*scale(
-      map(abs(velocity), 0, 64, 1.5, .69),
-      map(abs(velocity), 0, 64, .5, 1.31)
-    );*/
   } else if (pos > skyPos) {
-    //pos = skyPos;
     dir = -2;
     console.log(pos);
-  } else {
-    console.log(map(abs(velocity), 0, 100, 1, .5), map(abs(velocity), 0, 100, 1, 1.5))  ;
-    /*scale(
-      map(abs(velocity), 0, 100, 1, .5),
-      map(abs(velocity), 0, 100, 1, 1.5)
-    );*/
-    //scale(1, 1);
   }
 
+  // Display Title
+  if (frameCount < 120){
+    return;
+  } else if (frameCount == 120) {
+    document.getElementById('title1').style.display = 'none';
+  }
 
-  //translate(cubeSize/2, cubeSize/2, cubeSize/2);
-
-  // Adjust camera for debugging
-  //translate(0, -550, 0);
-
-  // Make floor for debugging
-  /*push();
-  translate(0,  500, -1000);
-  translate(0, cubeSize, 0);
-  box(1000, 10, 1000);
-  pop();*/
-  /*scale(
-    map(abs(velocity), 0, 100, 1, .5),
-    map(abs(velocity), 0, 100, 1, 1.5)
-  );*/
-  //translate(cubeSize/2, cubeSize/2, cubeSize/2);
+  // Translate cube based on position
   translate(0, -pos, -2000);
-  //translate(0, -cubeSize, 0);
+
+  // Translate cube to scale based on top
   translate(0, cubeSize, 0);
 
-
-
-  //translate(0, -pos, -1000);
-  //rotateZ(frameCount * 0.01);
-  //rotateX(frameCount * 0.01);
-
+  // Squash + Stretch
   if (pos == groundPos) {
-    if (abs(velocity) >= 56) {
+    if (abs(velocity) >= 90) {
       scale(
-        map(abs(velocity), 56, 112, 1.5, .5),
-        map(abs(velocity), 56, 112, .5, 1.5)
+        map(abs(velocity), 90, 112, 2, .5),
+        map(abs(velocity), 90, 112, .2, 1.5)
       );
     } else {
       scale(
-        //map(abs(velocity), 0, 32, 1, 1.5),
-        //map(abs(velocity), 0, 32, 1, .5)
-        map(abs(velocity), 0, 56, 1, 1.5),
-        map(abs(velocity), 0, 56, 1, .5)
+        map(abs(velocity), 0, 90, 1, 2),
+        map(abs(velocity), 0, 90, 1, .2)
       );
     }
-
   } else {
     scale(
       map(abs(velocity), 0, 112, 1, .5),
       map(abs(velocity), 0, 112, 1, 1.5)
     );
   }
-
-
 
   translate(0, -cubeSize, 0);
 
